dae39080618111ca5a3e3453219885bb811dbde2
ghadeerTek <ghadeertek@gmail.com>
2015-10-11 11:31:06 +0300

    Update module2_lesson1_formative.rb

--- Assignments/Lesson01-Assignment01-Case-Statement/solution/module2_lesson1_formative.rb
+++ Assignments/Lesson01-Assignment01-Case-Statement/solution/module2_lesson1_formative.rb
@@ -14,3 +14,8 @@ when some_var == false
 else
   puts "I guess nothing matched... But why?"
 end
+
+#Output : I guess nothing matched... But why?
+#The reason : the first test fails because it is obvious that some_var does not equal "pink elephant" .
+#The second and third ones fail because although another_var and some_var are nil and false and 
+#seem to achieve condition ,these are strings not real nil and false .
